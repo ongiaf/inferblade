8b08b8042f91971146ab3ea611ad89f52a2797ad
ongiaf <147675548+ongiaf@users.noreply.github.com>
2025-08-12 01:38:21 +0800

    adjust template html

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 import base64
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
-@app.route('/run_esm3_predict', methods=['POST'])
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/run_esm3_predict", methods=["POST"])
 def run_esm3_predict():
     payload = {}
-    payload['sequence'] = request.form.get('sequence','')
+    payload["sequence"] = request.form.get("esm3_sequence", "")
 
-    resp = requests.post('http://localhost:8000/mims/mock/esm3/predict', json=payload)
+    resp = requests.post("http://localhost:8000/mims/mock/esm3/predict", json=payload)
     data = resp.json()
     # pass outputs to template
-    return render_template('index.html', outputs=data)
+    return render_template("index.html", outputs=data)
 
 
-@app.route('/run_moflow_predict', methods=['POST'])
-def run_moflow_predict():
+@app.route("/run_moflow_generation", methods=["POST"])
+def run_moflow_generation():
     payload = {}
-    payload['seed'] = request.form.get('seed',0)
+    payload["seed"] = request.form.get("moflow_seed", 0)
 
-    resp = requests.post('http://localhost:8000/mims/mock/moflow/predict', json=payload)
+    resp = requests.post(
+        "http://localhost:8000/mims/mock/moflow/generation", json=payload
+    )
     data = resp.json()
     # pass outputs to template
-    return render_template('index.html', outputs=data)
+    return render_template("index.html", outputs=data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=6006, debug=True)
--- tests/mock_server/mock_server.py
+++ tests/mock_server/mock_server.py
@@ -3,31 +3,32 @@ import argparse
 
 # Simple HTTP mock server using Flask for local testing
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/mims/mock/esm3/predict', methods=['POST'])
+
+@app.route("/mims/mock/esm3/predict", methods=["POST"])
 def esm3_predict():
     j = request.get_json() or {}
-    seq = j.get('sequence','')
+    seq = j.get("sequence", "")
     with open("./tests/data/1a46_protein_processed.pdb", "r") as f:
         pdb = f.read().strip()
     pdb_b64 = base64.b64encode(pdb.encode()).decode()
-    return jsonify({'pdb': pdb_b64})
+    return jsonify({"pdb": pdb_b64})
 
 
-@app.route('/mims/mock/moflow/predict', methods=['POST'])
+@app.route("/mims/mock/moflow/generation", methods=["POST"])
 def moflow_predict():
     j = request.get_json() or {}
-    seed = j.get('seed','')
+    seed = j.get("seed", "")
     with open("./tests/data/1a46_ligand.sdf", "r") as f:
         sdf = f.read().strip()
     sdf_b64 = base64.b64encode(sdf.encode()).decode()
-    return jsonify({'sdf': sdf_b64})
+    return jsonify({"sdf": sdf_b64})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=8000)
+    parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
     app.run(port=args.port, debug=True)
-
