4b56effb290c6db6dbca75d949fb997212558f2e
ongiaf <147675548+ongiaf@users.noreply.github.com>
2025-08-12 01:58:19 +0800

    add model diffdock

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-import requests
 import base64
+
+import requests
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -10,12 +11,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/run_esm3_predict", methods=["POST"])
+@app.route("/run_esm3_prediction", methods=["POST"])
 def run_esm3_predict():
     payload = {}
     payload["sequence"] = request.form.get("esm3_sequence", "")
 
-    resp = requests.post("http://localhost:8000/mims/mock/esm3/predict", json=payload)
+    resp = requests.post(
+        "http://localhost:8000/mims/mock/esm3/prediction", json=payload
+    )
     data = resp.json()
     # pass outputs to template
     return render_template("index.html", outputs=data)
@@ -28,6 +31,20 @@ def run_moflow_generation():
 
     resp = requests.post(
         "http://localhost:8000/mims/mock/moflow/generation", json=payload
+    )
+    data = resp.json()
+    # pass outputs to template
+    return render_template("index.html", outputs=data)
+
+
+@app.route("/run_diffdock_prediction", methods=["POST"])
+def run_diffdock_prediction():
+    payload = {}
+    payload["pdb"] = request.form.get("diffdock_pdb", "")
+    payload["sdf"] = request.form.get("diffdock_sdf", "")
+
+    resp = requests.post(
+        "http://localhost:8000/mims/mock/diffdock/prediction", json=payload
     )
     data = resp.json()
     # pass outputs to template
--- tests/mock_server/mock_server.py
+++ tests/mock_server/mock_server.py
@@ -1,16 +1,15 @@
-import base64
 import argparse
+import base64
 
 # Simple HTTP mock server using Flask for local testing
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route("/mims/mock/esm3/predict", methods=["POST"])
-def esm3_predict():
+@app.route("/mims/mock/esm3/prediction", methods=["POST"])
+def esm3_prediction():
     j = request.get_json() or {}
-    seq = j.get("sequence", "")
     with open("./tests/data/1a46_protein_processed.pdb", "r") as f:
         pdb = f.read().strip()
     pdb_b64 = base64.b64encode(pdb.encode()).decode()
@@ -18,13 +17,24 @@ def esm3_predict():
 
 
 @app.route("/mims/mock/moflow/generation", methods=["POST"])
-def moflow_predict():
+def moflow_generation():
     j = request.get_json() or {}
-    seed = j.get("seed", "")
     with open("./tests/data/1a46_ligand.sdf", "r") as f:
         sdf = f.read().strip()
     sdf_b64 = base64.b64encode(sdf.encode()).decode()
     return jsonify({"sdf": sdf_b64})
+
+
+@app.route("/mims/mock/diffdock/prediction", methods=["POST"])
+def diffdock_prediction():
+    j = request.get_json() or {}
+    with open("./tests/data/1a46_protein_processed.pdb", "r") as f:
+        pdb = f.read().strip()
+    pdb_b64 = base64.b64encode(pdb.encode()).decode()
+    with open("./tests/data/1a46_ligand.sdf", "r") as f:
+        sdf = f.read().strip()
+    sdf_b64 = base64.b64encode(sdf.encode()).decode()
+    return jsonify({"pdb": pdb_b64, "sdf": sdf_b64})
 
 
 if __name__ == "__main__":
